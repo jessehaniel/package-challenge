dc7da45cbb931ba065e562803d2ad6d59d36dbff
06071721423 <jesse.haniel@tjpb.jus.br>
2019-07-11 20:49:49 -0300

    Tester project  exemplifying consumption of Packer module

--- tester/src/main/java/com/mobiquityinc/tester/Application.java
+++ tester/src/main/java/com/mobiquityinc/tester/Application.java
@@ -2,9 +2,10 @@ package com.mobiquityinc.tester;
 
 import com.mobiquityinc.packer.Packer;
 import com.mobiquityinc.packer.exception.APIException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,14 +13,36 @@ public class Application {
     
     private static Logger logger = LogManager.getLogger(Application.class);
     
-    public static void main(String[] args) throws APIException {
-        String testFile = "";
-        String answer = Optional.ofNullable(Packer.pack(testFile)).orElse("No response");
-        String outputFile = "";
-        try (FileWriter writer = new FileWriter(outputFile)){
-            writer.write(answer);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
+    public static void main(String[] args) {
+        new Application().execute();
+    }
+    
+    private void execute() {
+        String filePath = prepareInputFile(getControlInput());
+        logger.info("Starting Packer for input set\n{}", getControlInput());
+        try {
+            String solution = Packer.pack(filePath);
+            logger.info("Execution finished with solution\n{}", solution);
+        } catch (APIException e) {
+            logger.error("Error trying to solve Knapsack", e);
         }
+    }
+    
+    private String prepareInputFile(String fileContent) {
+        String filePath = System.getProperty("java.io.tmpdir") + "testFile.txt";
+        try {
+            Path newPath = Files.write(Paths.get(filePath), fileContent.getBytes());
+            return newPath.toString();
+        } catch (IOException e) {
+            logger.error("Error trying to write test input file", e);
+        }
+        return "";
+    }
+    
+    private String getControlInput() {
+        return "81 : (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48)\n"
+            + "8 : (1,15.3,€34)\n"
+            + "75 : (1,85.31,€29) (2,14.55,€74) (3,3.98,€16) (4,26.24,€55) (5,63.69,€52) (6,76.25,€75) (7,60.02,€74) (8,93.18,€35) (9,89.95,€78)\n"
+            + "56 : (1,90.72,€13) (2,33.80,€40) (3,43.15,€10) (4,37.97,€16) (5,46.81,€36) (6,48.77,€79) (7,81.80,€45) (8,19.36,€79) (9,6.76,€64)\n";
     }
 }
